5183d36fdf82982e17ee5db901f87b8679c0e59c
Chong-Yee Khoo <mail@cykhoo.com>
2014-02-09 12:41:39 +0800

    restructure factories

--- spec/factories/exchange_rate.rb
+++ spec/factories/exchange_rate.rb
@@ -2,10 +2,10 @@ FactoryGirl.define do
   factory :exchange_rate, aliases: [:usd_exchange_rate], class: SingDollar::ExchangeRate do
     bank_buying  { FactoryGirl.build(:transaction_usd_buying) }
     bank_selling { FactoryGirl.build(:transaction_usd_selling) }
+  end
 
-    factory :eur_exchange_rate do
-      bank_buying  { FactoryGirl.build(:transaction_eur_buying) }
-      bank_selling { FactoryGirl.build(:transaction_eur_selling) }
-    end
+  factory :eur_exchange_rate, class: SingDollar::ExchangeRate do
+    bank_buying  { FactoryGirl.build(:transaction_eur_buying) }
+    bank_selling { FactoryGirl.build(:transaction_eur_selling) }
   end
 end
--- spec/factories/transaction.rb
+++ spec/factories/transaction.rb
@@ -1,27 +1,29 @@
 FactoryGirl.define do
-  factory :transaction, aliases: [:transaction_usd_buying], class: SingDollar::Transaction do
+  factory :transaction_usd_buying, aliases: [:transaction], class: SingDollar::Transaction do
     currency :usd
     type     :bank_buying
     tt       1.2592
     od       1.2542
+  end
 
-    factory :transaction_usd_selling do
-      type     :bank_selling
-      tt       1.2787
-      od       1.2788
-    end
+  factory :transaction_usd_selling, class: SingDollar::Transaction do
+    currency :usd
+    type     :bank_selling
+    tt       1.2787
+    od       1.2788
+  end
 
-    factory :transaction_eur_buying do
-      currency :eur
-      type     :bank_buying
-      tt       1.2592
-      od       1.2542
+  factory :transaction_eur_buying, class: SingDollar::Transaction do
+    currency :eur
+    type     :bank_buying
+    tt       1.2592
+    od       1.2542
+  end
 
-      factory :transaction_eur_selling do
-        type     :bank_selling
-        tt       1.7480
-        od       1.7480
-      end
-    end
+  factory :transaction_eur_selling, class: SingDollar::Transaction do
+    currency :eur
+    type     :bank_selling
+    tt       1.7480
+    od       1.7480
   end
 end
