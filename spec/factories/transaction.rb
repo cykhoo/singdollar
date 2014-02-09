23e29717be71fa2a6540ea67571bb36ad4fd7761
Chong-Yee Khoo <mail@cykhoo.com>
2014-02-09 12:33:51 +0800

    rename transactions to use transaction_ prefix

--- spec/factories/transaction.rb
+++ spec/factories/transaction.rb
@@ -1,23 +1,23 @@
 FactoryGirl.define do
-  factory :transaction, aliases: [:usd_buying], class: SingDollar::Transaction do
+  factory :transaction, aliases: [:transaction_usd_buying], class: SingDollar::Transaction do
     currency :usd
     type     :bank_buying
     tt       1.2592
     od       1.2542
 
-    factory :usd_selling do
+    factory :transaction_usd_selling do
       type     :bank_selling
       tt       1.2787
       od       1.2788
     end
 
-    factory :eur_buying do
+    factory :transaction_eur_buying do
       currency :eur
       type     :bank_buying
       tt       1.2592
       od       1.2542
 
-      factory :eur_selling do
+      factory :transaction_eur_selling do
         type     :bank_selling
         tt       1.7480
         od       1.7480
