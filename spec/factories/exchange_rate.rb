bc25242b48964e0a3193bac046416185da2dbca5
Chong-Yee Khoo <mail@cykhoo.com>
2014-02-09 12:34:13 +0800

    actually build transaction objects (instead of symbols!)

--- spec/factories/exchange_rate.rb
+++ spec/factories/exchange_rate.rb
@@ -1,11 +1,11 @@
 FactoryGirl.define do
   factory :exchange_rate, aliases: [:usd_exchange_rate], class: SingDollar::ExchangeRate do
-    bank_buying  :usd_buying
-    bank_selling :usd_selling
+    bank_buying  { FactoryGirl.build(:transaction_usd_buying) }
+    bank_selling { FactoryGirl.build(:transaction_usd_selling) }
 
     factory :eur_exchange_rate do
-      bank_buying  :eur_buying
-      bank_selling :eur_selling
+      bank_buying  { FactoryGirl.build(:transaction_eur_buying) }
+      bank_selling { FactoryGirl.build(:transaction_eur_selling) }
     end
   end
 end
