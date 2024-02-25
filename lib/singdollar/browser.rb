3cb2dfa1175a1bafc686e76f6b6168f2042d155a
Chong-Yee Khoo <mail@cykhoo.com>
2024-02-25 13:07:56 +0000

    add chrome options
    allow chromedriver to work within docker. error:
    session not created: Chrome failed to start: exited normally.
    (Selenium::WebDriver::Error::SessionNotCreatedError)
    (session not created: DevToolsActivePort file doesn't exist)
    
    https://bugs.chromium.org/p/chromedriver/issues/detail?id=2473

--- lib/singdollar/browser.rb
+++ lib/singdollar/browser.rb
@@ -15,7 +15,9 @@ module SingDollar
     end
 
     Capybara.register_driver :headless_chrome do |app|
-      chrome_options.add_argument '--headless'
+      chrome_options.add_argument('--headless')
+      chrome_options.add_argument('--no-sandbox')
+      chrome_options.add_argument('--disable-dev-shm-usage')
       Capybara::Selenium::Driver.new(app, browser: :chrome, options: chrome_options)
     end
 
